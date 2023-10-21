213265af5d16e360dab37427df4f876cdf59cbd1
nicos1985 <nicolasferratto@hotmail.com>
2023-10-21 10:58:37 -0300

    agrego debug toolbar. corrijo error de jquery

--- passing/settings.py
+++ passing/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
 
 # Application definition
 
@@ -40,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'passbase',
     'login',
-    'permission'
+    'permission',
+    'debug_toolbar',
     
 ]
 
@@ -52,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
@@ -153,3 +160,19 @@ AUTH_USER_MODEL = 'login.CustomUser'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
--- passing/urls.py
+++ passing/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -21,6 +22,13 @@ urlpatterns = [
     path('',include('login.urls') ),
     path('admin/', admin.site.urls),
     path('pass/',include('passbase.urls') ),
-    path('perm/',include('permission.urls') )
+    path('perm/',include('permission.urls') ),
+    path("__debug__/", include("debug_toolbar.urls")),
     
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += path("__debug__/", include("debug_toolbar.urls")),
+                   
