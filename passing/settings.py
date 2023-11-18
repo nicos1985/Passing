a967ffd809d544f9bce490c27469aa0d4583e48a
nicos1985 <nicolasferratto@hotmail.com>
2023-11-18 14:20:13 -0300

    postgre database

--- passing/settings.py
+++ passing/settings.py
@@ -96,9 +96,14 @@ WSGI_APPLICATION = 'passing.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Antares313$',
+        'HOST': 'localhost',  # Puedes cambiarlo según tu configuración.
+        'PORT': '5432',           # Deja vacío para usar el valor predeterminado.
     }
 }
 
