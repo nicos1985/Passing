d157e2d6e6cb391a330c7781756037288c845b5a
nicos1985 <nicolasferratto@hotmail.com>
2024-05-12 08:59:07 -0300

    admin changes

--- passbase/admin.py
+++ passbase/admin.py
@@ -6,12 +6,15 @@ from .models import Contrasena, LogData, SeccionContra
 # Register your models here.
 
 class ContrasenaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_contra' , 'seccion', 'info' ,'usuario')
     readonly_fields=('created', 'updated')
 
 class LogDataAdmin(admin.ModelAdmin):
+    list_display = ('contraseña', 'entidad', 'usuario', 'action', 'created')
     readonly_fields=('created',)
     
 class SeccionContAdmin(admin.ModelAdmin):
+    list_display= ('nombre_seccion',)
     readonly_fields=('created', 'updated')
 
 admin.site.register(SeccionContra ,SeccionContAdmin)
