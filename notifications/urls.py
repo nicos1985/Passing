130ac4190c51020f936559f617e947a2fc7c66cf
nicos1985 <nicolasferratto@hotmail.com>
2024-05-18 14:10:44 -0300

    restriccion de permisos de acceso a notificaciones

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from notifications.views import CreateNotificationsUser, ListNotificationsAdmin, ListNotificationsUser, UpdateNotificationsAdmin, UpdateNotificationsUser, share_contrasena_form
+from notifications.views import  ListNotificationsAdmin, ListNotificationsUser, UpdateNotificationsAdmin, UpdateNotificationsUser, share_contrasena_form
 
 
 urlpatterns = [
     
-    #path('create-notif-user', CreateNotificationsUser.as_view(), name='createnotifuser'),
     path('create-notif-admin/<int:contrasena>', share_contrasena_form, name='createnotiadmin'),
     #path('list-notif-user', ListNotificationsUser.as_view(), name='listnotifuser'),
     path('list-notif-admin', ListNotificationsAdmin.as_view(), name='listnotifadmin'),
--- notifications/views.py
+++ notifications/views.py
@@ -1,44 +1,42 @@
 from django.contrib import messages
 from django.db.models.query import QuerySet
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from login.models import CustomUser
 from notifications.forms import CreateNotificationForm
 from notifications.models import AdminNotification, UserNotifications
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import user_passes_test
 
 from passbase.models import Contrasena
 from permission.models import ContraPermission
 
+def can_view_contrasena(user, request):
+    contrasena_id = int(request.GET.get('contrasena'))
+    contrasena_obj = get_object_or_404(Contrasena, id=contrasena_id)
+    return user.is_staff or ContraPermission.objects.filter(user_id=user, contra_id=contrasena_obj, permission=True).exists()
 
+def is_administrator(user):
+    return user.is_superuser
 
-# Create your views here.
-class CreateNotificationsUser():
-    pass
-
-
+#@user_passes_test(can_view_contrasena)
 def share_contrasena_form(request, contrasena):
     if request.method == 'POST':
-        # Procesa el formulario si es una solicitud POST
         form = CreateNotificationForm(request.POST)
         if form.is_valid():
-            
-            # Si el formulario es válido, procesa los datos y guarda en el modelo UserNotifications
             share = form.cleaned_data['id_user_share']
             comment = form.cleaned_data['comment']
-            contrasena_obj = Contrasena.objects.get(id=contrasena)
+            contrasena_obj = get_object_or_404(Contrasena, id=contrasena)
+
             obj_permission = ContraPermission.objects.filter(user_id=share, contra_id=contrasena, permission=True).first()
-            #chequeo que ese usuario no tenga ya ese permiso consedido.
             if not obj_permission:
-                # Crea una instancia de AdminNotifications y guarda los datos en la base de datos
                 notificacion, created = AdminNotification.objects.get_or_create(
                     id_contrasena=contrasena_obj,
-                    id_user_share=share, 
+                    id_user_share=share,
                     defaults={
-                        'id_user': request.user.username, 
-                        'type_notification': 'Compartir Contraseña', 
+                        'id_user': request.user.username,
+                        'type_notification': 'Compartir Contraseña',
                         'action': 'Permiso a contraseña',
                         'comment': comment,
                         'viewed': False,
@@ -48,35 +46,37 @@ def share_contrasena_form(request, contrasena):
                     message = f'Se ha solicitado compartir la contraseña "{contrasena_obj.nombre_contra}" con el usuario {share}.'
                     messages.success(request, message)
                 else:
-                    message = f'Ya existe una solicitud para compartir la contraseña "{contrasena_obj.nombre_contra}" con el usuario {share}. Si no fue atendida la solicitud comuníquese con el administrador'
+                    message = f'Ya existe una solicitud para compartir la contraseña "{contrasena_obj.nombre_contra}" con el usuario {share}. Si no fue atendida la solicitud comuníquese con el administrador.'
                     messages.warning(request, message)
             else:
                 message = f'El usuario {share} ya puede visualizar la contraseña "{contrasena_obj.nombre_contra}".'
                 messages.success(request, message)
-            # Redirige a una página de éxito o a donde sea necesario
+            
             return redirect('listpass')
         else:
-            # Muestra los errores del formulario
             messages.error(request, 'Hay errores en el formulario, por favor verifica los campos.')
     else:
-        # Si no es una solicitud POST, simplemente muestra el formulario vacío
         form = CreateNotificationForm()
-
+    
     return render(request, 'create-noti-admin.html', {'form': form})
-
-
 
 class ListNotificationsUser():
     pass
 
 
-class ListNotificationsAdmin(LoginRequiredMixin, ListView):
+class ListNotificationsAdmin(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = AdminNotification
     template_name = 'admin-noti-list.html'
     context_object_name = 'notifications'
     login_url = 'login'
     ordering = ['viewed', '-created']
+
+    def test_func(self):
+        return is_administrator(self.request.user)
     
+    def handle_no_permission(self):
+        messages.error(self.request, "No tienes permiso para acceder a esta página.")
+        return redirect('listpass')  # Redirigir a la página de inicio u otra página adecuada
 
 class UpdateNotificationsUser():
     pass
