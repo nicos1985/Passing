22e8d26ffc4ba65a851fe92e0a35de9d97ce4bc4
nicos1985 <nicolasferratto@hotmail.com>
2024-05-24 09:42:55 -0300

    fix navbar. Notification Bell

--- passing/context_processor.py
+++ passing/context_processor.py
@@ -1,11 +1,13 @@
 from notifications.models import AdminNotification, UserNotifications
 
 def counter_admin_notifications(request):
-    contador = AdminNotification.objects.filter(type_user=1, viewed=False).count()
     if request.user.is_authenticated:
-        contador_notis_user = UserNotifications.objects.filter(id_user = request.user, viewed=False).count()
+        if request.user.is_superuser:
+            contador_notis = AdminNotification.objects.filter(type_user=1, viewed=False).count()
+        else:
+            contador_notis= UserNotifications.objects.filter(id_user = request.user, viewed=False).count()
     else:
-        contador_notis_user = 0
-    return {'contador_notis': contador, 'contador_notis_user' : contador_notis_user}
+        contador_notis = 0
+    return {'contador_notis': contador_notis}
 
 
