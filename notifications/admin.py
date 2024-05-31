181304cc2293d8bb29b4f06e955463a1d6af6600
nicos1985 <nicolasferratto@hotmail.com>
2024-05-31 14:38:07 -0300

    admin notifications2

--- notifications/admin.py
+++ notifications/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 # Register yofrom .models import CustomUser
 from .models import UserNotifications, AdminNotification
 class UserNotificationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'id_user', 'id_contra')
+    list_display = ('id', 'id_user', 'id_contrasena')
 
 class AdminNotificationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'id_user', 'id_user_share', 'id_contra')
+    list_display = ('id', 'id_user', 'id_user_share', 'id_contrasena')
 
 
-admin.site.register(UserNotifications ,AdminNotification, UserNotificationsAdmin, AdminNotificationsAdmin ) #ur models here.
+admin.site.register(UserNotifications ,UserNotificationsAdmin) #ur models here.
+admin.site.register(AdminNotification, AdminNotificationsAdmin)
