9cb9863eacade0e527f350b2825f6ff92168e9bf
nicos1985 <nicolasferratto@hotmail.com>
2024-08-29 17:20:50 -0300

    fixing email sending

--- login/urls.py
+++ login/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', views.register , name='register'),
     path('logout/', LogoutFormView.as_view(), name='logout'),
     path('profile/<str:username>/', views.profile_view, name='profile'),
-    path('reset-password/', CustomPasswordResetView.as_view(template_name = 'password_reset.html'), name='password_reset'),
+    path('reset-password/', PasswordResetView.as_view(template_name = 'password_reset.html'), name='password_reset'),
     path('reset-password/done/', PasswordResetDoneView.as_view(template_name='reset_password_done.html'), name='password_reset_done'),
     path('reset-password/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset-password/complete/', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
