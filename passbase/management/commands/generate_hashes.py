af81186c7bbba100250f0593a7d065cab93c94fa
nicos1985 <nicolasferratto@hotmail.com>
2024-09-09 10:43:35 -0300

    fix generate hashes

--- passbase/management/commands/generate_hashes.py
+++ passbase/management/commands/generate_hashes.py
@@ -9,23 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         contrasenas = Contrasena.objects.all()
         for contrasena in contrasenas:
-            if contrasena.hash is None:  # Sólo procesa si el hash está vacío
-                usuario = contrasena.usuario
-                password = contrasena.contraseña
-                
-                # Desencripta el usuario y la contraseña si están encriptados
-                try:
-                    usuario_decrypted = decrypt_data(usuario)
-                    self.stdout.write(self.style.SUCCESS(f'usuario desencriptado: {usuario_decrypted}'))
-                    password_decrypted = decrypt_data(password)
-                    self.stdout.write(self.style.SUCCESS(f'usuario desencriptado: {password_decrypted}'))
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f'Error desencriptando para id {contrasena.id}: {e}'))
-                    continue
+        # Sólo procesa si el hash está vacío
+            usuario = contrasena.usuario
+            password = contrasena.contraseña
+            
+            # Desencripta el usuario y la contraseña si están encriptados
+            try:
+                usuario_decrypted = decrypt_data(usuario)
+                self.stdout.write(self.style.SUCCESS(f'usuario desencriptado: {usuario_decrypted}'))
+                password_decrypted = decrypt_data(password)
+                self.stdout.write(self.style.SUCCESS(f'usuario desencriptado: {password_decrypted}'))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(f'Error desencriptando para id {contrasena.id}: {e}'))
+                continue
 
-                # Genera el hash
-                hash_combination = hashlib.sha256((usuario_decrypted + password_decrypted).encode('utf-8')).hexdigest()
-                
-                contrasena.hash = hash_combination
-                contrasena.save()
-                self.stdout.write(self.style.SUCCESS(f'Hash generado para Contrasena ID {contrasena.id}'))
+            # Genera el hash
+            hash_combination = hashlib.sha256((usuario_decrypted + password_decrypted).encode('utf-8')).hexdigest()
+            
+            contrasena.hash = hash_combination
+            contrasena.save()
+            self.stdout.write(self.style.SUCCESS(f'Hash generado para Contrasena ID {contrasena.id}'))
