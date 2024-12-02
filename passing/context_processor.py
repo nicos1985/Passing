bc8c8c049f93f54b5bc6cba8c7f55a8b3fb84a7e
nicos1985 <nicolasferratto@hotmail.com>
2024-12-02 09:38:58 -0300

    fix staff notifications

--- passing/context_processor.py
+++ passing/context_processor.py
@@ -4,6 +4,8 @@ def counter_admin_notifications(request):
     if request.user.is_authenticated:
         if request.user.is_superuser:
             contador_notis = AdminNotification.objects.filter(type_user=1, viewed=False).count()
+        elif request.user.is_staff:
+            contador_notis = AdminNotification.objects.filter(type_user=1, viewed=False).count()
         else:
             contador_notis= UserNotifications.objects.filter(id_user = request.user, viewed=False).count()
     else:
